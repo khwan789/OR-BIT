b0d8a5ae3be634be2d67e306a4b9243de9e804df
최현 <hyun@pear-tree.io>
2025-04-02 00:53:13 +0900

    tw

--- Assets/Scripts/GPGSIds.cs
+++ Assets/Scripts/GPGSIds.cs
@@ -1,5 +1,5 @@
 public static class GPGSIds
 {
-	public const string leaderboard_high_score = ""; // 리더보드 ID
+	public const string leaderboard_high_score = "CgkI6-CJi5YfEAIQAA"; // 리더보드 ID
 	public const string achievement_first_win = ""; // 업적 ID
 }
